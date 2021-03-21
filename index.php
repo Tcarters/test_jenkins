6ebdc629001243329886eb4bd37528870c98f8dd
Tcarters <71230412+Tcarters@users.noreply.github.com>
2021-03-21 06:32:00 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<body bgcolor='aqua'>
+  
+  <hr> Welcome to the  Testing with OC <hr/>
+  
+  
+  <?php>
+  
+  print "Web testing with php ......................";
+  <br>
+    
+    print `ifconfig`;
+  <br>
+    
+    print "Bye !!!!!!!!!!!...............";
+  ?>
+  
+  Coucou.......................
